df741e31c2242b79d1891193749337d2206b471f
SegoB5M <20240621@leon.tecnm.mx>
2024-09-17 09:47:00 -0600

    Modificación numero 2
    Se muestra al usuario el área alculada

--- CalculoArea.py
+++ CalculoArea.py
@@ -1,6 +1,7 @@
 # Solicitar al usuario que ingrese el ancho y la altura del rectángulo
 ancho = float(input("Ingrese el ancho del rectángulo: "))
 altura = float(input("Ingrese la altura del rectángulo: "))
+
 area = ancho * altura
 
 print(f"El área del rectángulo es: {area}")
